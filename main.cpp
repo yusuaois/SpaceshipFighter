45981f552464d4e27ab11c39ada74001ff8490d0
AnchialC <yusuaois@outlook.com>
2025-02-28 17:05:30 +0800

    SDL框架

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_init.h>
+#include <SDL3/SDL_oldnames.h>
 #include <SDL3/SDL_render.h>
 #include <iostream>
 
@@ -12,15 +13,35 @@ int main(int, char **) {
     return 1;
   }
 
-  //创建窗口
-  SDL_Window *window = SDL_CreateWindow("SDL3",800,600, 0);
+  // 创建窗口
+  SDL_Window *window = SDL_CreateWindow("SDL3", 800, 600, 0);
 
-  //创建渲染器
+  // 创建渲染器
   SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
 
-  //SDL3_Image不需要初始化，会自动初始化
+  // 渲染循环
+  while (true) {
+    SDL_Event event;
+    if (SDL_PollEvent(&event)) {
+      if (event.type == SDL_EVENT_QUIT) {
+        break;
+      }
+    }
 
-  //清理
+    // 清屏
+    SDL_RenderClear(renderer);
+
+    //画一个长方形
+    SDL_FRect rect = {100, 100, 200, 200};
+    SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+    SDL_RenderFillRect(renderer, &rect);
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+    // 更新屏幕
+    SDL_RenderPresent(renderer);
+  }
+
+  // 清理
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
   SDL_Quit();
